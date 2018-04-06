63ab6180460d5c15f1dfd5472df7e3736b670b78
Roland Willis <rolandwillisiv@gmail.com>
2018-04-06 14:30:43 -0700

    further tweaked style, made contact button animations, added content to gallery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,21 @@
-$(".icon").click(function(){
+$("#gmail-link").toggle();
+$("#linkedin-link").toggle();
+$("#github-link").toggle();
+
+$("#gmail-btn").click(function(){
     $("#gmail-link").animate({
         width: 'toggle'
     });
-}); 
+});
+
+$("#linkedin-btn").click(function(){
+    $("#linkedin-link").animate({
+        width: 'toggle'
+    });
+});
+
+$("#github-btn").click(function(){
+    $("#github-link").animate({
+        width: 'toggle'
+    });
+});
